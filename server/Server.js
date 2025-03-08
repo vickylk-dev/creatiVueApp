f8ae2fa0dc0998a83f3394e9e7d9b3686e0c3233
vignesh.l <vignesh.l@cavininfotech.com>
2025-03-08 17:30:28 +0530

    vervel 3

--- server/Server.js
+++ server/Server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const {Server} = require('socket.io');
 const cors = require('cors');
 
+const port = process.env.PORT || 5000;
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -50,10 +52,7 @@ io.on("connection", (socket) => {
   });
 });
 
-// server.listen(5000, ()=> {
-//     console.log('Server is running on port 5000');
-// })
+server.listen(port, () => console.log(`Server running on port ${port}`));
 
-module.exports = app;
 
 
